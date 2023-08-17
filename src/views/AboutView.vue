a8e19b2ba73d5daa1747b2a7ec9d70789f029a0e
AyoubMS <chahayb563@gmail.com>
2023-08-17 17:27:24 +0200

    add the composition API

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {useFlash} from "@/composables/useFlash.js";
+
+let {flash} = useFlash()
+
+</script>
+
+<template>
+  <main>
+    <p>
+      <button @click="flash('Yay!', 'It Works On the About Page!', 'info')">Click Me!</button>
+    </p>
+  </main>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {useFlash} from "@/composables/useFlash.js";
+
+let {flash} = useFlash()
+
+</script>
+
+<template>
+  <main>
+    <p>
+      <button @click="flash('Test', 'It Works!')">Click Me!</button>
+    </p>
+  </main>
+</template>
